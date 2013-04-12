d3bc74431f5c9caea507eeed8ef7999b3a25884f
yangjiandong <young.jiandong@gmail.com>
2013-04-12 15:02:13 +0800

    oo

--- dubbo-ekapp-consumer/src/main/java/org/ssh/dubbo/demo/consumer/DemoAction.java
+++ dubbo-ekapp-consumer/src/main/java/org/ssh/dubbo/demo/consumer/DemoAction.java
@@ -4,11 +4,12 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-import org.springframework.stereotype.Component;
 import org.ssh.dubbo.demo.DemoService;
+import org.ssh.dubbo.demo.NurseMobService;
 import org.ssh.dubbo.demo.model.Hz;
 
-import com.alibaba.dubbo.config.annotation.Reference;
+import com.ek.mobileapp.model.OrderData;
+import com.ek.mobileapp.model.Patient;
 
 //@Component
 public class DemoAction {
@@ -16,12 +17,18 @@ public class DemoAction {
     //@Reference(version="1.0.0")
     private DemoService<Hz> demoService;
 
+    NurseMobService nurseMobService;
+
+    public void setNurseMobService(NurseMobService nurseMobService) {
+        this.nurseMobService = nurseMobService;
+    }
+
     public void setDemoService(DemoService<Hz> demoService) {
         this.demoService = demoService;
     }
 
     public void start() throws Exception {
-        for (int i = 0; i < Integer.MAX_VALUE; i ++) {
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
             try {
                 String hello = demoService.sayHello("world" + i);
                 System.out.println("[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "] " + hello);
@@ -30,6 +37,17 @@ public class DemoAction {
                 List<Hz> alls = demoService.getPageItems(i + 1);
                 for (Hz hz : alls) {
                     System.out.println(hz.getHz());
+                }
+
+                //get patients
+                List<Patient> ps = this.nurseMobService.getPatientAll(4L, "H2028");
+                for (Patient patient : ps) {
+                    System.out.println(patient.getPatientId() + "," + patient.getPatientName());
+
+                    List<OrderData> ods = this.nurseMobService.getOrderDataFromHisTest(patient.getPatientId());
+                    for (OrderData orderData : ods) {
+                        System.out.println(">>>>>" + orderData.getOrderNo() + "," + orderData.getOrderText());
+                    }
                 }
             } catch (Exception e) {
                 e.printStackTrace();
